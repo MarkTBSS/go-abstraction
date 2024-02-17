5a5c15bcaad7bdb75dd4e19879de1af2a7d4617a
MarkTBSS <marktbss@gmail.com>
2024-02-17 16:09:25 +0700

    first commit

---
+++ warrior/warrior.go
@@ -0,0 +1,38 @@
+package warrior
+
+import (
+	"fmt"
+
+	"github.com/MarkTBSS/go-abstraction/novice"
+)
+
+type IWarrior interface {
+	DisplayInfo()
+	DeleteHealth(damage int)
+}
+
+// Warrior struct embeds the Novice struct and adds a color field
+type Warrior struct {
+	*novice.Novice // Embedding the *novice.Novice pointer
+	color          string
+}
+
+// NewWarrior creates a new instance of Warrior
+func NewWarrior(name, classname string, health int, color string) IWarrior {
+	return &Warrior{
+		Novice: novice.NewNovice(name, classname, health).(*novice.Novice), // Convert to *novice.Novice
+		color:  color,
+	}
+}
+
+// DisplayInfo displays the information of the Warrior including the color
+func (w *Warrior) DisplayInfo() {
+	w.Novice.DisplayInfo() // Calling DisplayInfo method of the embedded Novice struct
+	fmt.Println("Color Changed:", w.color)
+}
+
+// DeleteHealth decreases the player's health by half the given damage
+func (w *Warrior) DeleteHealth(damage int) {
+	halfDamage := damage / 2
+	w.Novice.DeleteHealth(halfDamage)
+}
